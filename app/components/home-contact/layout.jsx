5b4e29b9ae3b6639fceaf8e25aae58859a5be962
josec17 <josec@openquire.com>
2024-10-11 13:41:35 -0600

    Hyprpunk contact us form

--- app/components/home-contact/layout.jsx
+++ app/components/home-contact/layout.jsx
@@ -1,29 +1,29 @@
 export default function ContactSections() {
-    return (
-      <section id="contact" className="py-28 bg-[var(--black)] animate-border border-t-2">
-        <div className="max-w-7xl mx-auto px-4">
-          <h2 className="text-2xl md:text-3xl font-bold text-center">Let's Talk About Your Project</h2>
-          <p className="mt-4 text-sm md:text-base text-center">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue sed nulla in finibus. 
-            Maecenas sed dolor tortor. Praesent a tempor tortor, ac commodo felis.
+  return (
+    <section id="contact" className="py-28 bg-[var(--black)] animate-border border-t-2">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="max-w-xl mx-auto text-center px-4">
+          <h2 className="text-2xl md:text-3xl font-bold">Get in Touch with Us</h2>
+          <p className="mt-4 text-sm md:text-base">
+            We're here to help you bring your ideas to life! Whether you have a project in mind, need assistance with our services, or simply want to say hello, we're just a message away. Please fill out the form below, and we’ll respond promptly to discuss your needs and how we can assist you.
           </p>
-          <div className="mt-8 flex justify-center gap-8">
-            <div className="w-full md:w-1/2">
-              <div className="flex justify-center">
-                <iframe
-                  src="https://us11.list-manage.com/contact-form?u=f32c2eedf5acaf8a022afbe47&form_id=93489f86ab790742b1a70708373faddb"
-                  width="100%"
-                  height="800"
-                  frameBorder="0"
-                  scrolling="no"
-                  className="rounded-lg border-2 border-gray-300"
-                  title="Contact Us Form"
-                ></iframe>
-              </div>
+        </div>
+        <div className="mt-8 flex justify-center">
+          <div className="w-full md:w-1/2 min-w-[300px]">
+            <div className="flex justify-center">
+              <iframe
+                src="https://us11.list-manage.com/contact-form?u=f32c2eedf5acaf8a022afbe47&form_id=93489f86ab790742b1a70708373faddb"
+                width="100%"
+                height="850"
+                frameBorder="0"
+                scrolling="no"
+                className="rounded-lg"
+                title="Contact Us Form"
+              ></iframe>
             </div>
           </div>
         </div>
-      </section>
-    );
-  }
-  
+      </div>
+    </section>
+  );
+}
